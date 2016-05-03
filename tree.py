8c3fb9e5fbe2bbc95bb4bcf0c315e79a63f57b34
Alceu Bissoto <abissoto@daitangroup.com>
2016-05-03 00:44:42 -0300

    Added getSizeTree and selectNode

--- tree.py
+++ tree.py
@@ -32,9 +32,7 @@ class BinaryNode(object):
             self.size += 1
             return True
         elif self.arity > 0:
-            counter = 0
             for child in self.children:
-                counter = counter + 1
                 if child.addBinaryNode(binaryNode):
                     return True
         else:
@@ -50,7 +48,8 @@ def generateRandomTree(listOperations, listTerminals):
         decisionMaking = random.randint(1, 100)
         if emptyTerminals == 0:
             break
-        elif decisionMaking < 53:
+        #The probability of adding a terminal needs to be bigger than the one to add a function.
+        elif decisionMaking < 40:
            random_Operation = random.randrange(0,len(listOperations))
            if newNode.addBinaryNode(BinaryNode(listOperations[random_Operation][0], listOperations[random_Operation][1], [])):
                emptyTerminals += listOperations[random_Operation][1] - 1
@@ -65,12 +64,39 @@ def generateRandomTree(listOperations, listTerminals):
         return generateRandomTree(listOperations, listTerminals)
     return newNode
 
+def getSizeTree(t1):
+    counter = len(t1.children)
+    for child in t1.children:
+        counter += getSizeTree(child)
+    return counter
+
+def selectNode(t1, counter):
+    if counter == 0:
+        return t1
+    else:
+       for child in t1.children:
+           if getSizeTree(child)+1 >= counter:
+               return selectNode(child, counter-1)
+           else:
+              counter -= getSizeTree(child)+1
+
 lO = [['+',2],['*',2],['-',2],['/',2]]
 lT = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
+geno = []
+#for i in range(1, 10):
+#    newNode = generateRandomTree(lO, lT)
+#    geno.append(newNode)
+
+#for i in range(1, 10):
+#    print geno[i-1], "with size: ", geno[i-1].size
+
+
+
 new = generateRandomTree(lO, lT) 
-print new
-print 'EVALUATION: ', new.evaluate()
+print new, "size: ", getSizeTree(new)
+print selectNode(new, 3)
+#print 'EVALUATION: ', new.evaluate()
 #node = BinaryNode('*', 2)
 #node.addBinaryNode(BinaryNode(10, 0, []))
 #node.addBinaryNode(BinaryNode('+', 2, []))
